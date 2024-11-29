0acc51865cd306804e29afcdc5263b0c741594c9
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-11-29 20:58:57 +0530

    Update CORS configuration to allow requests from production domain

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(errorHandeller);
 
 // CORS setup with options
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://vrv-securityrbac-taskround.onrender.com',
   credentials: true,
 }));
 
